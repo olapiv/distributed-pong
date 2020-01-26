7b251a4b0ef14dbebc4445ea1c53b3b5ec935811
Goradux <abbbo@mail.ru>
2020-01-26 04:13:51 +0100

    speed changes

--- frontend/pong1970s.js
+++ frontend/pong1970s.js
@@ -10,11 +10,12 @@ const intervalLength = 30;
 const dxy = speed*intervalLength;
 let dx = dxy;
 let dy = -dxy;
+let paddleConstant = 5;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-let paddle_height = 40;
+let paddle_height = 60;
 let paddle_width = 10;
 let y_paddle = canvas.height / 2;
 let x_paddle = 30;
@@ -42,8 +43,6 @@ function sendMessage(y, angle) {
 function sendGameOverMessage() {
     socket.send("LOST");
 }
-
-
 
 function ballEdgeCollisionDetector() {
     if(ball_x + dx > canvas.width-ballRadius && ballOnOurSide) {
@@ -123,10 +122,10 @@ function keyUpHandler(e) {
 
 function paddleControls() {    
     if(upPressed) {
-        y_paddle = y_paddle-5;
+        y_paddle = y_paddle-paddleConstant;
     }
     else if(downPressed) {
-        y_paddle = y_paddle+5;
+        y_paddle = y_paddle+paddleConstant;
     }
 }
 
@@ -164,6 +163,9 @@ function draw() {
     paddleBallCollisionDetector();
     ball_x += dx;
     ball_y += dy;
+    dx = dx * 1.0001;
+    dy = dy * 1.0001;
+    paddleConstant = paddleConstant * 1.0001;
 }
 
 drawingInterval = setInterval(draw, intervalLength);
